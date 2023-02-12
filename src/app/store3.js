af5128e28f8a80de969aafedc0d95079a52d89cf
ismail <ismailsardar@gmail.com>
2023-02-12 23:08:22 +0600

    myCounter app sliceReduser add store

--- src/app/store3.js
+++ src/app/store3.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import myCounterReducer from '../features/myCounter/myCounterSlice'
 import postReducer from '../features/posts/postSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
+    myCounter: myCounterReducer
   },
 })
