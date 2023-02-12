8af1a2b36244c5e8d8e329d9d6e34f5147df1eba
ismail <ismailsardar@gmail.com>
2023-02-13 00:50:29 +0600

    use api reduser add it make by ismile sardar

--- src/app/store3.js
+++ src/app/store3.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import myCounterReducer from '../features/myCounter/myCounterSlice'
 import postReducer from '../features/posts/postSlice'
+import userReduser from '../features/users/userSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
-    myCounter: myCounterReducer
+    myCounter: myCounterReducer,
+    users: userReduser
   },
 })
